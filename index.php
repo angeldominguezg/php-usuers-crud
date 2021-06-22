ff81b1d0d1128b19c4a94964ac8cc36f8d42bc68
Angel Dominguez <angeldominguezg@gmail.com>
2021-06-21 21:05:40 -0400

    Card for edit user

--- index.php
+++ index.php
@@ -5,8 +5,6 @@ $users =  getUsers();
 ?>
 
 <div class="container mx-auto px-4">
-
-    <!-- Test info in cards -->
     <div class="grid sm:grid-cols-1 md:grid-cols-4 gap-6">
     <?php foreach($users as $user): ?>
         <div class="card border-2 border-gray-300 rounded-lg px-2 py-2">
@@ -25,10 +23,10 @@ $users =  getUsers();
             </div>
             <div class="card-footer text-center my-3">
                 <div class="inline-flex">
-                    <a href="view.php?id=<?php echo $user['id']; ?>" class="bg-blue-400 text-white hover:bg-gray-400 font-bold py-1 px-4 rounded-l">
+                    <a href="view.php?id=<?php echo $user['id'] ?>" class="bg-blue-400 text-white hover:bg-gray-400 font-bold py-1 px-4 rounded-l">
                         View
                     </a>
-                    <a href="edit.php?id=<?php echo $user['id']; ?>" class="bg-green-400 text-white hover:bg-gray-400 font-bold py-1 px-4">
+                    <a href="edit.php?id=<?php echo $user['id'] ?>" class="bg-green-400 text-white hover:bg-gray-400 font-bold py-1 px-4">
                         Edit
                     </a>
                     <form action="delete.php" method="post">
@@ -40,47 +38,6 @@ $users =  getUsers();
         </div>
     <?php endforeach; ?>
     </div>
-    <!-- End Test info in cards -->
-
-
-    <!-- <a class="btn btn-primary" href="create.php">Add User</a> -->
-    <!-- <table class="table-auto">
-        <thead>
-            <tr class="bg-gray-100">
-                <th class="px-4 py-2">Name</th>
-                <th class="px-4 py-2">Username</th>
-                <th class="px-4 py-2">Email</th>
-                <th class="px-4 py-2">Phone</th>
-                <th class="px-4 py-2">Website</th>
-                <th class="px-4 py-2">Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach($users as $user): ?>
-            <tr class="bg-gray-100">
-                <td class="px-4 py-2"><?php echo $user['name']; ?></td>
-                <td class="px-4 py-2"><?php echo $user['username']; ?></td>
-                <td class="px-4 py-2"><?php echo $user['email']; ?></td>
-                <td class="px-4 py-2"><?php echo $user['phone']; ?></td>
-                <td class="px-4 py-2"><?php echo $user['website']; ?></td>
-                <td class="px-4 py-2">
-                    <div class="inline-flex">
-                        <a href="view.php?id=<?php echo $user['id']; ?>" class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l">
-                            View
-                        </a>
-                        <a href="edit.php?id=<?php echo $user['id']; ?>" class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4">
-                            Edit
-                        </a>
-                        <form action="delete.php" method="post">
-                            <input type="hidden" name="id" value="<?php echo $user['id'] ?>">
-                            <button type="submit" class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r">Delete</button>
-                        </form>
-                    </div>
-                </td>
-            </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table> -->
 </div>
 
 <?php
--- view.php
+++ view.php
@@ -8,35 +8,50 @@ $user =  getUserByID($id);
 // echo '</pre>';
 ?>
 <content>
-    <div class="container">
-        <h1>User User</h1>
-        <div>
-            <label for="">Name: </label>
-            <em><?php echo $user['name']?></em>
-        </div>
-        <div>
-            <label for="">User Name: </label>
-            <em><?php echo $user['username']?></em>
-        </div>
-        <div>
-            <label for="">Email: </label>
-            <em><?php echo $user['email']?></em>
-        </div>
-        <div>
-            <label for="">Phone: </label>
-            <em><?php echo $user['phone']?></em>
-        </div>
-        <div>
-            <label for="">Website: </label>
-            <em><?php echo $user['website']?></em>
-        </div>
+    <div class="container mx-auto px-4">
 
-        <!-- Update -->
-        <a href="edit.php?id=<?php echo $user['id'] ?>">Edit</a>
-        <!-- Delete -->
-        <a href="delete.php?id=<?php echo $user['id'] ?>">Delete</a>
-        <!-- Back -->
-        <a href="index.php">Back</a>
+        <div class="card w-96 shadow-2xl rounded-lg mx-auto">
+            <div class="card-header relative h-20 bg-gradient-to-r from-green-400 to-blue-500 rounded-t-lg">
+                <h1 class="absolute top-5 left-4 font-extrabold text-4xl right-0 text-white">#<?php echo $user['id']; ?></h1>
+                <img class="absolute top-4 right-4 w-20 rounded-full border-4 border-green-300" src="img/no-photo.jpg" alt="No user photo available">
+            </div>
+
+            <div class="card-body px-4 pt-4 pb-2 space-y-4">
+                <div>
+                    <label for="">Name: </label>
+                    <em><?php echo $user['name']?></em>
+                </div>
+                <div>
+                    <label for="">User Name: </label>
+                    <em><?php echo $user['username']?></em>
+                </div>
+                <div>
+                    <label for="">Email: </label>
+                    <em><?php echo $user['email']?></em>
+                </div>
+                <div>
+                    <label for="">Phone: </label>
+                    <em><?php echo $user['phone']?></em>
+                </div>
+                <div>
+                    <label for="">Website: </label>
+                    <em><?php echo $user['website']?></em>
+                </div>
+            </div>
+
+            <div class="card-footer text-center my-3 pb-4">
+                <div class="inline-flex">
+                    <a href="edit.php?id=<?php echo $user['id'] ?>" class="bg-green-400 text-white hover:bg-gray-400 font-bold py-1 px-6 rounded-l">
+                        Edit
+                    </a>
+                    <form action="delete.php" method="post">
+                        <input type="hidden" name="id" value="<?php echo $user['id'] ?>">
+                        <button type="submit" class="bg-red-400 text-white hover:bg-gray-400 font-bold py-1 px-4 rounded-r">Delete</button>
+                    </form>
+                </div>
+            </div>
+
+        </div>
     </div>
 </content>
 <?php
