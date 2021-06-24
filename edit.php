684be924020398251cf9bac51173822b856974b8
Angel Dominguez <angeldominguezg@gmail.com>
2021-06-24 18:55:56 -0400

    Styles for edit user

--- edit.php
+++ edit.php
@@ -11,34 +11,61 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 ?>
 <content>
-    <div class="container">
-        <h1>Edit User: <?php echo $user['name']?></h1>
+    <div class="container mx-auto px-4">
+
+        <div class="card w-96 shadow-2xl rounded-lg mx-auto">
+            <div class="card-header relative h-20 bg-gradient-to-r from-green-400 to-blue-500 rounded-t-lg">
+                <h1 class="absolute top-5 left-4 font-extrabold text-4xl right-0 text-white">#<?php echo $user['id']; ?></h1>
+                <img class="absolute top-4 right-4 w-20 rounded-full border-4 border-green-300" src="img/no-photo.jpg" alt="No user photo available">
+            </div>
+
+            <form action="edit.php?id=<?php echo $id ?>" method="post">
+                <div class="card-body px-4 pt-4 pb-2 space-y-4">
+                    <input type="hidden" name="id" value="<?php echo $user['id']?>">
+                    <div>
+                        <label for="name">Name</label>
+                        <div class="mt-1">
+                            <input class="w-full border border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500" type="text" name="name" value="<?php echo $user['name']?>">
+                        </div>
+                    </div>
+                    <div>
+                        <label for="user-name">User Name</label>
+                        <div class="mt-1">
+                            <input class="w-full border border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500" type="text" name="username" value="<?php echo $user['username']?>">
+                        </div>
+                    </div>
+                    <div>
+                        <label for="email">Email</label>
+                        <div class="mt-1">
+                            <input class="w-full border border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500" type="text" name="email" value="<?php echo $user['email']?>">
+                        </div>
+                    </div>
+                    <div>
+                        <label for="phone">Phone</label>
+                        <div class="mt-1">
+                            <input class="w-full border border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500" type="text" name="phone" value="<?php echo $user['phone']?>">
+                        </div>
+                    </div>
+                    <div>
+                        <label for="website">Website</label>
+                        <div class="mt-1">
+                            <input class="w-full border border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500" type="text" name="website" value="<?php echo $user['website']?>">
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer text-center my-3 pb-4">
+                    <div class="inline-flex">
+                        <a href="index.php" class="bg-gray-400 text-white hover:bg-gray-400 font-bold py-1 px-6 rounded-l">
+                            Back
+                        </a>
+                        <button type="submit" class="bg-blue-400 text-white hover:bg-gray-400 font-bold py-1 px-4 rounded-r">Update</button>
+                    </div>
+                </div>
+            </form>
+
+        </div>
     </div>
-    <form action="edit.php?id=<?php echo $id ?>" method="post">
-            <input type="hidden" name="id" value="<?php echo $user['id']?>">
-            <div>
-                <label for="name">Name</label>
-                <input class="bg-gray-200" type="text" name="name" value="<?php echo $user['name']?>">
-            </div>
-            <div>
-                <label for="username">User Name</label>
-                <input class="bg-gray-200" type="text" name="username" value="<?php echo $user['username']?>">
-            </div>
-            <div>
-                <label for="email">Email</label>
-                <input class="bg-gray-200" type="text" name="email" value="<?php echo $user['email']?>">
-            </div>
-            <div>
-                <label for="phone">Phone</label>
-                <input class="bg-gray-200" type="text" name="phone" value="<?php echo $user['phone']?>">
-            </div>
-            <div>
-                <label for="website">Website</label>
-                <input class="bg-gray-200" type="text" name="website" value="<?php echo $user['website']?>">
-            </div>
-            <button type="submit">Update User</button>
-        </form>
 </content>
 <?php
-    require './partials/footer.php';
+require './partials/footer.php';
 ?>
